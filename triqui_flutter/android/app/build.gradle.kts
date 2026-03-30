4fc79c57b2b827bf4093b2c54d74c42818f8048b
gizmo <niquimiqui@gmail.com>
2026-03-30 15:25:38 -0500

    Update versioning in pubspec.yaml and build.gradle.kts to version 1.0.0+3 and 1.2 respectively, reflecting the latest app updates.

--- triqui_flutter/android/app/build.gradle.kts
+++ triqui_flutter/android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         // Versión mínima: Android 5.0 (Lollipop)
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     signingConfigs {
